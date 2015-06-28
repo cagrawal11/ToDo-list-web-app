17a3342a7cf38bd3f6d03148f69ad9818713bad3
Chetan Agrawal <cagrawal11@gmail.com>
2015-06-28 13:01:28 +0530

    gitignore

--- ListApp/views.py
+++ ListApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+def index_view(request):
+    '''First page of our app. This form just renders the first page, where one
+    can register, or log in.'''
+    if request.method == 'GET':
+        return render(request, '/index.html', {})
