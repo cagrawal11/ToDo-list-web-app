37755372966926c0b49ceb0af799c3557509d0ae
Chetan Agrawal <cagrawal11@gmail.com>
2015-07-04 23:43:57 +0530

    first working page

--- ListApp/urls.py
+++ ListApp/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^index$', views.index_view, name='index')
+	url(r'^$', views.index_view, name='index')
 )
--- ListApp/views.py
+++ ListApp/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 
 
 # Create your views here.
 
 def index_view(request):
-    '''First page of our app. This form just renders the first page, where one
-    can register, or log in.'''
-    if request.method == 'GET':
-        return render_request('/index.html') 
+	#return HttpResponse('hello world')
+	return render_to_response("index.html")
