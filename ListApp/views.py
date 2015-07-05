9871f75c6754600dbc1ffcceb9b45cd6e4f65d90
Chetan Agrawal <cagrawal11@gmail.com>
2015-07-05 23:08:36 +0530

    AddTask initial- added timestamp

--- ListApp/views.py
+++ ListApp/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 
-import models
+import models, datetime
 # Create your views here.
 
 def index_view(request):
 	#return HttpResponse('hello world')
-	return render_to_response("index.html", {'data1':': I m just learning now..', 'date':'timestamp'})
+	return render_to_response("index.html", {'data1':': I m just learning now..', 'date': datetime.datetime.utcnow()})
 
 
 def add_task(request):
-	'''task_description = request.POST['taskToDo']
+	task_description = request.POST['taskToDo']
 	models.AddTask(TaskDecription = task_description)
-	models.AddTask.save()'''
+	models.AddTask.save()
 	return render_to_response("addTask.html")
